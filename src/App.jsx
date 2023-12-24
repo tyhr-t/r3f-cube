854fac2c1fb1601fc7a8c37ef7b65d23f5bbecec
tidiane <kreyze@macbook-air-de-tidiane.home>
2023-12-24 19:30:28 +0100

    cube movements

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import "./App.css";
+import { useRef } from "react";
+
+const Cube = ({ position, size, color }) => {
+  const ref = useRef();
+  useFrame((state, delta) => {
+    ref.current.rotation.x += delta;
+    ref.current.rotation.y += delta;
+  });
+  return (
+    <mesh position={position} ref={ref}>
+      <boxGeometry args={size} />
+      <meshBasicMaterial color={color} />
+    </mesh>
+  );
+};
 
 function App() {
   return (
     <Canvas>
-      <mesh>
-        <boxGeometry />
-        <meshBasicMaterial color="red" />
-      </mesh>
+      <directionalLight position={[0, 0, 2]} />
+      <Cube position={[1, 0, 0]} color={"green"} size={[1, 1, 1]} />
     </Canvas>
   );
 }
